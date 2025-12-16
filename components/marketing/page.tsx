0975e21325b19153aed83b28e20ee443352f2891
scottwelch968 <scottblackandcompany@gmail.com>
2025-12-15 21:29:02 -0600

    Create MarketingPage component with initial layout

---
+++ components/marketing/page.tsx
@@ -0,0 +1,8 @@
+export default function MarketingPage() {
+  return (
+    <main style={{ padding: 32 }}>
+      <h1>Spork Marketing Preview</h1>
+      <p>Wireframe sections go here next.</p>
+    </main>
+  );
+}
